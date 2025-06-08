0988f853d6112767f9613675a2ba96be13c36ac3
GatikM <gatikmadaan1809@gmail.com>
2025-06-08 22:18:56 +0530

    Basics of Node JS

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log("Hello, world!");
+
+const catMe = require('cat-me');
+
+console.log(catMe());
+
+// What is npm?
+
+// npm stands for Node Package Manager. It is a package manager for the JavaScript programming language, primarily used for managing packages in Node.js applications. npm allows developers to install, share, and manage dependencies in their projects easily. It provides a vast repository of open-source libraries and tools that can be integrated into applications, streamlining the development process.
