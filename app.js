77e2b56207ef4ddf5b58b1a310df80fd3421bf45
GatikM <gatikmadaan1809@gmail.com>
2025-06-09 01:42:22 +0530

    Worked on different types of middleware

--- app.js
+++ app.js
@@ -1,16 +1,38 @@
-const express = require('express');
+const express = require("express");
+const morgan = require("morgan"); // this is a third-party middleware for logging HTTP
+// requests in Node.js applications.
 const app = express();
 
-app.get('/', (req,res) => {
-    res.send('Hello World!');
-})
+app.use(morgan("dev"));
+app.set("view engine", "ejs");
 
-app.get('/about', (req, res) =>{
-    res.send('This is the about page');
-})
+//Middleware to log requests
+app.use((req, res, next) => {
+  //console.log("This is a middleware function");
+  // this is a custom middleware function that logs the request method and URL.
+  return next();
+});
 
-app.get('/profile', (req,res) => {
-    res.send('This is the profile page');
-})
+app.get("/", (req, res) => {
+  res.send("Hello this is our default route");
+});
+
+app.get(
+  "/about",
+  (req, res, next) => { // running a middleware for a specific route
+    const a = 10;
+    const b = 20;
+    console.log(a + b);
+
+    next();
+  },
+  (req, res) => {
+    res.render("index");
+  }
+);
+
+app.get("/profile", (req, res) => {
+  res.send("This is the profile page");
+});
 
 app.listen(3000);
